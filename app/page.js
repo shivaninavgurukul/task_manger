0eea8c0e3975479c69296d8708c56a73aaa335a3
shivani mehta <shivanimehta21@navgurukul.org>
2023-11-04 12:54:18 +0530

    todo list

---
+++ app/page.js
@@ -0,0 +1,16 @@
+"use client"
+import React from "react";
+
+
+import "./globals.css";
+import Functionality from "./Functionality";
+
+const page = () => {
+  return (
+    <div>
+      <Functionality />
+    </div>
+  );
+}
+
+export default page;
